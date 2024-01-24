d30c6db8abf320cc7ba45dc4ad3040091107f32a
joe parr <joe.parr@hotmail.co.uk>
2024-01-24 16:16:21 +0000

    datenight

---
+++ Mini-Projects/dateNight.py
@@ -0,0 +1,11 @@
+# Define a list of possible cuisines
+cuisines = ['Italian', 'Japanese', 'Mexican', 'Chinese', 'Indian', 'Thai', 'French', 'Mediterranean', 'Korean', 'Vietnamese']
+
+# Import random library
+import random
+
+# Select a random cuisine from the list
+selected_cuisine = random.choice(cuisines)
+
+# Print the selected cuisine
+print("Selected cuisine: " + selected_cuisine)
